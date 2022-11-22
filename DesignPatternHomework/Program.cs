b94576e7ec2e6809aea30e22c03be84a3f071bd7
Mehe_od76@itstep.edu.az <Mehe_od76@itstep.edu.az>
2022-11-23 00:02:28 +0400

    Добавьте файлы проекта.

---
+++ DesignPatternHomework/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Homework1;
+
+public interface IButton
+{
+    void Render();
+    void OnClick();
+}
+
+public class WindowsButton : IButton
+{
+    public void OnClick() => Console.WriteLine("Windows button Click");
+    public void Render() => Console.WriteLine("Windows button Render");
+}
+
+public class WebButton : IButton
+{
+    public void OnClick() => Console.WriteLine("Web button Click");
+    public void Render() => Console.WriteLine("Web render Render");
+}
+
+
+public abstract class Dialog
+{
+    public void render()
+    {
+        IButton okButton = CreateButton();
+        okButton.OnClick();
+        okButton.Render();
+    }
+
+    public abstract IButton CreateButton();
+}
+
+public class WindowsDialog : Dialog
+{
+    public override IButton CreateButton() => new WindowsButton();
+}
+
+public class HtmlDialog : Dialog
+{
+    public override IButton CreateButton() => new WebButton();
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Dialog MAUI = new WindowsDialog();
+        MAUI.render();
+        MAUI = new HtmlDialog();
+        MAUI.render();
+    }
+}
---
+++ Homework2/Program.cs
@@ -0,0 +1,123 @@
+﻿namespace Homework2;
+
+public interface IChair
+{
+    void HasLegs();
+    void SitOn();
+}
+public class VictorianChair : IChair
+{
+    public void HasLegs() => Console.WriteLine("Has Legs");
+    public void SitOn() => Console.WriteLine("Sit on VictorianChair");
+}
+
+public class ModernChair : IChair
+{
+    public void HasLegs() => Console.WriteLine("Has no Legs");
+    public void SitOn() => Console.WriteLine("Sit on ModernChair");
+}
+
+public class ArtDecoChair : IChair
+{
+    public void HasLegs() => Console.WriteLine("Has Legs");
+    public void SitOn() => Console.WriteLine("Sit on ArtDecoChair");
+}
+
+public interface ICoffeeTable
+{
+    void IsRound();
+    void PlaceOn();
+}
+
+public class VictorianCoffeeTable : ICoffeeTable
+{
+    public void IsRound() => Console.WriteLine("Is Round");
+    public void PlaceOn() => Console.WriteLine("Placed on Victorian");
+}
+
+public class ModernCoffeeTable : ICoffeeTable
+{
+    public void IsRound() => Console.WriteLine("Is Round");
+    public void PlaceOn() => Console.WriteLine("Placed on {nameof(ModernCoffeeTable)}");
+}
+
+
+public class ArtDecoCoffeeTable : ICoffeeTable
+{
+    public void IsRound() => Console.WriteLine("Is Not Round");
+    public void PlaceOn() => Console.WriteLine("Placed on ArtDecoCoffeeTable");
+}
+
+public interface ISofa
+{
+    void HasLegs();
+    void SitOn();
+}
+public class VictorianSofa : ISofa
+{
+    public void HasLegs() => Console.WriteLine("Has Legs");
+    public void SitOn() => Console.WriteLine("Sat on VictorianSofa");
+
+}
+
+public class ModernSofa : ISofa
+{
+    public void HasLegs() => Console.WriteLine("Has No Legs");
+    public void SitOn() => Console.WriteLine("Sat on ModernSofa");
+}
+
+public class ArtDecoSofa : ISofa
+{
+    public void HasLegs() => Console.WriteLine("Has Legs");
+    public void SitOn() => Console.WriteLine("Sat on ArtDecoSofa");
+
+}
+
+public abstract class FurnitureFactory
+{
+    public abstract IChair CreateChair();
+    public abstract ISofa CreateSofa();
+    public abstract ICoffeeTable CreateCoffeeTable();
+}
+
+public class VictorianFurnitureFactory : FurnitureFactory
+{
+    public override IChair CreateChair() => new VictorianChair();
+
+    public override ICoffeeTable CreateCoffeeTable() => new VictorianCoffeeTable();
+
+    public override ISofa CreateSofa() => new VictorianSofa();
+}
+
+public class ModernFurnitureFactory : FurnitureFactory
+{
+    public override IChair CreateChair() => new ModernChair();
+
+    public override ICoffeeTable CreateCoffeeTable() => new ModernCoffeeTable();
+
+    public override ISofa CreateSofa() => new ModernSofa();
+}
+
+public class ArtDecoFurnitureFactory : FurnitureFactory
+{
+    public override IChair CreateChair() => new ArtDecoChair();
+
+    public override ICoffeeTable CreateCoffeeTable() => new ArtDecoCoffeeTable();
+
+    public override ISofa CreateSofa() => new ArtDecoSofa();
+}
+
+public class Program
+{
+    static void Main()
+    {
+        FurnitureFactory factory = new VictorianFurnitureFactory();
+        factory.CreateChair().SitOn();
+
+        factory = new ModernFurnitureFactory();
+        factory.CreateChair().SitOn();
+
+        factory = new ArtDecoFurnitureFactory();
+        factory.CreateChair().SitOn();
+    }
+}
